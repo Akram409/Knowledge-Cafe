30d0f40eaededad66c6f129610bcb1a4baaf5870
RaW <akram.iiuc.ctg@gmail.com>
2023-03-31 16:25:11 +0600

    NavBar Finish

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import NavBar from './Component/NavBar/NavBar';
 
 const App = () => {
   return (
     <div>
-      
+      <NavBar></NavBar>
     </div>
   );
 };
---
+++ src/Component/NavBar/NavBar.jsx
@@ -0,0 +1,50 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <div>
+      <div className="navbar bg-base-100">
+        <div className="flex-1">
+          <a className="btn btn-ghost normal-case text-2xl font-bold">
+            Knowledge Cafe
+          </a>
+        </div>
+        <div className="flex-none gap-2">
+        <div>
+          <ul className="flex gap-10 me-9 text-black text-xl list-none font-bold">
+            <li>Home</li>
+            <li>About</li>
+            <li>Contact</li>
+          </ul>
+        </div>
+          <div className="dropdown dropdown-end">
+            <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+              <div className="w-10 rounded-full">
+                <img src="/Author.jpg" />
+              </div>
+            </label>
+            <ul
+              tabIndex={0}
+              className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52"
+            >
+              <li>
+                <a className="justify-between">
+                  Profile
+                  <span className="badge">New</span>
+                </a>
+              </li>
+              <li>
+                <a>Settings</a>
+              </li>
+              <li>
+                <a>Logout</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NavBar;
